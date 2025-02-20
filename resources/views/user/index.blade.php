d48f517388694da7d65abf2e65e39e3f754a1eab
Schemple <ngohuuhieut66@gmail.com>
2025-02-20 11:04:17 +0700

    repository first touch

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,13 @@
+<?php
+$x = 7;
+?>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+</head>
+<body class="font-sans antialiased dark:bg-black dark:text-white/50">
+<div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
+    <img id="background" class="absolute -left-20 top-0 max-w-[877px]" src="https://laravel.com/assets/img/welcome/background.svg" alt="Laravel background" />
+</div>
+</body>
+</html>
